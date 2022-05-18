3899feccba4f7342d7fbce3ba5430b0f0bb52389
wallycoding <wallysonbreno.dev@gmail.com>
2022-05-18 11:02:59 -0300

    feature: follow isFollowing

--- src/services/user/profile/usePublicProfileService.ts
+++ src/services/user/profile/usePublicProfileService.ts
@@ -2,6 +2,11 @@ import type { Request, Response } from "express";
 import database from "../../../database";
 
 const usePublicProfileService = async (req: Request, res: Response) => {
+  const {
+    user: {
+      profile: { id: profileID },
+    },
+  } = req.props;
   const { id } = req.params;
 
   const user = await database.profile.findUnique({
@@ -23,7 +28,19 @@ const usePublicProfileService = async (req: Request, res: Response) => {
     },
   });
 
-  res.json(user);
+  const isFollowing = !!await database.follow.findUnique({
+    where: {
+      following_id_follow_id: {
+        follow_id: id,
+        following_id: profileID,
+      },
+    },
+  });
+
+  res.json({
+    ...user,
+    isFollowing,
+  });
 };
 
 export default usePublicProfileService;
